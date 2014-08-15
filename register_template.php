9a306c39486870e60cda9d90a3e6716d690dba86
ZainSohail <zain110mb@gmail.com>
2014-08-15 17:46:00 +0500

    Removed page display bug

--- register_template.php
+++ register_template.php
@@ -6,30 +6,19 @@ add_action("template_redirect", 'my_theme_redirect');
 function my_theme_redirect() {
     global $wp;
     $plugindir = dirname( __FILE__ );
-	$pageid = get_the_ID();
-	$pagename = get_query_var('pagename');
-
+    $pagename ='personal-study' . '/' . get_query_var('pagename');
+		
      //A Specific Custom Post Type
     if ($wp->query_vars["post_type"] == 'personal-study') {
         $templatefilename = 'BarGraph.php';
-        if (file_exists(TEMPLATEPATH . '/' . $templatefilename)) {
+		if (file_exists(TEMPLATEPATH . '/' . $templatefilename)) {
             $return_template = TEMPLATEPATH . '/' . $templatefilename;
         } else {
             $return_template = $plugindir . '/themefiles/' . $templatefilename;
         }
         do_theme_redirect($return_template);
     } 
-	
-	if ($wp->query_vars["page_id"] == $pageid) {
-        $templatefilename = 'BarGraph.php';
-        if (file_exists(TEMPLATEPATH . '/' . $templatefilename)) {
-            $return_template = TEMPLATEPATH . '/' . $templatefilename;
-        } else {
-            $return_template = $plugindir . '/themefiles/' . $templatefilename;
-        }
-        do_theme_redirect($return_template);
-    } 
-	
+
 	if ($wp->query_vars["pagename"] == $pagename) {
         $templatefilename = 'BarGraph.php';
         if (file_exists(TEMPLATEPATH . '/' . $templatefilename)) {
@@ -37,7 +26,7 @@ function my_theme_redirect() {
         } else {
             $return_template = $plugindir . '/themefiles/' . $templatefilename;
         }
-        theme_redirect_search($return_template);
+        do_theme_redirect($return_template);
     }
 }
 
